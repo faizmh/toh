21fb59520bde5cc721c42ff45c7eb993119cabf5
Faiz M Haneef <faizmh@neoito.com>
2017-12-14 22:10:31 +0530

    You created a separate, reusable HeroDetailComponent. You used a property binding to give the parent HeroesComponent control over the child HeroDetailComponent. You used the @Input decorator to make the hero property available for binding by the external HeroesComponent.

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -8,11 +8,38 @@ import { Hero } from '../hero';
 })
 export class HeroDetailComponent implements OnInit {
 
-  @Input() hero: Hero;
+  private _hero: Hero;
+
+  
+  get hero() {
+    return this._hero;
+  }
+
+  @Input()
+  set hero( hero: Hero) {
+    // to catch undefined hero 
+    if(!hero) return;
+    
+    this._hero = hero;
+    this._hero.name = 'XX' + this._hero.name; 
+  }
+  private _user:any;
+  
+  get user() {
+    return this._user;
+  }
+  @Input() 
+  set user( user:any) {
+    // to catch undefined user 
+    if(!user) return;
+    this._user = user;
+  }
   constructor() { }
 
   ngOnInit() {
     // this.selectedHero = this.hero;
+    // Angular 2: Do something with @Input inside Component
+    // https://stackoverflow.com/questions/34568213/angular-2-do-something-with-input-inside-component
   }
 
 }
